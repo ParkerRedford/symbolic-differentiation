2dced01d357dceed72b134b28775c4d937be64a9
Parker Redford <redrevyol@gmail.com>
2025-08-04 04:13:59 -0400

    .gitignore

--- target/debug/deps/symbolic_differentation-71678e72269f0525.d
+++
@@ -1,8 +0,0 @@
-C:\Users\Parker\Desktop\symbolic-differentation\target\debug\deps\symbolic_differentation-71678e72269f0525.d: src\main.rs src\expr\mod.rs src\expr\expr.rs src\expr\builder.rs
-
-C:\Users\Parker\Desktop\symbolic-differentation\target\debug\deps\libsymbolic_differentation-71678e72269f0525.rmeta: src\main.rs src\expr\mod.rs src\expr\expr.rs src\expr\builder.rs
-
-src\main.rs:
-src\expr\mod.rs:
-src\expr\expr.rs:
-src\expr\builder.rs:
